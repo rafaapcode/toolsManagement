7deffdb38fe16d4c407dd135f13859b5ab1bce65
rafaapcode <rafaap2003@gmail.com>
2022-11-02 11:36:28 -0300

    auth

--- src/routes/ToolsRoutes.js
+++ src/routes/ToolsRoutes.js
@@ -1,14 +1,14 @@
 import Router from 'express';
 import ToolsControler from '../controllers/ToolsControllers.js';
 import { ErrorsTools } from '../middlewares/Errors.js';
-
+import auth from '../middlewares/Auth.js';
 
 const toolRouter = new Router();
 
 toolRouter.get('/', ToolsControler.getAll);
 toolRouter.get('/:tag', ErrorsTools.getErrorgHandling, ToolsControler.get);
-toolRouter.post('/', ErrorsTools.storageErrorgHandling, ToolsControler.storage);
-toolRouter.delete('/:id', ErrorsTools.deleteErrorgHandling, ToolsControler.delete);
-toolRouter.put('addTag/:id', ErrorsTools.updateErrorgHandling, ToolsControler.updatedTags);
+toolRouter.post('/', auth, ErrorsTools.storageErrorgHandling, ToolsControler.storage);
+toolRouter.delete('/:id', auth, ErrorsTools.deleteErrorgHandling, ToolsControler.delete);
+toolRouter.put('addTag/:id', auth, ErrorsTools.updateErrorgHandling, ToolsControler.updatedTags);
 
 export default toolRouter;
