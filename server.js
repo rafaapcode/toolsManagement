677f38bafafcca86ada731b1ea39753091c06658
rafaapcode <rafaap2003@gmail.com>
2022-11-02 06:15:42 -0300

    No Cluster

--- server.js
+++ server.js
@@ -1,13 +1,3 @@
 import app from './app';
-import cluster from 'cluster';
-import os from 'os';
-const cpuLength = os.cpus().length;
 
-if (cluster.isPrimary) {
-    for (let i = 0; i < cpuLength; i++) {
-        cluster.fork();
-    };
-} else if (cluster.isWorker) {
-
-    app.listen(app.get('port'), () => console.log(`Listening on port ${app.get('port')}`))
-}
+app.listen(app.get('port'), () => console.log(`Listening on port ${app.get('port')}`))
